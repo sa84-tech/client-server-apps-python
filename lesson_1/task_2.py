f807907dbf1f0004d57a1b18ca62f878dafc5c2d
sa84 <sa84@list.ru>
2021-11-15 01:10:11 +0300

    lesson 1 start

---
+++ lesson_1/task_2.py
@@ -0,0 +1,14 @@
+"""
+Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования в последовательность кодов
+(не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
+"""
+from task_1 import display_format
+
+
+b1 = b'class'
+b2 = b'function'
+b3 = b'method'
+
+display_format(b1)
+display_format(b2)
+display_format(b3)
