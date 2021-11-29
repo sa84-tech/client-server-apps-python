1ddeb9add199db7c0b5e4b04cdf67efabee56650
sa84 <sa84@list.ru>
2021-11-30 02:03:23 +0300

    server added

--- lesson_3/messanger/server.py
+++ lesson_3/messanger/server.py
@@ -1,10 +1,36 @@
-from socket import *
+import socket
+import sys
+import json
 
-s = socket(AF_INET, SOCK_DGRAM)
-s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+from utils.constants import DEFAULT_PORT, MAX_CONNECTIONS
+from utils.message import get_message
 
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-s.bind(('', 8887))
-while True:
-    msg = s.recv(128)
-    print(msg)
+
+class Server:
+    def __init__(self, ip_address='', port=DEFAULT_PORT):
+        self.ip_address = ip_address
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __str__(self):
+        return f'Server is running on port {self.port}'
+
+    def listen(self):
+        self.socket.bind((self.ip_address, self.port))
+        self.socket.listen(MAX_CONNECTIONS)
+        print(self)
+
+        while True:
+            client, client_addr = self.socket.accept()
+            try:
+                message = get_message(client)
+                print(message)
+            except (ValueError, json.JSONDecodeError):
+                print('Bad request from client')
+            finally:
+                client.close()
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.listen()
