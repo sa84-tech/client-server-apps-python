08b48d052528f30acb785f131535235bf59ab59b
sa84 <sa84@list.ru>
2021-11-30 01:10:19 +0300

    constants added

---
+++ lesson_3/messanger/server.py
@@ -0,0 +1,10 @@
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+s.bind(('', 8887))
+while True:
+    msg = s.recv(128)
+    print(msg)
