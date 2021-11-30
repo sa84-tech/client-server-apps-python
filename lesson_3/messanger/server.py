03e410f12482311a943067809928097e823a9bf5
Андрей Громов <sa84@list.ru>
2021-11-30 16:47:06 +0300

    task for lesson3 completed

--- lesson_3/messanger/server.py
+++ lesson_3/messanger/server.py
@@ -2,18 +2,29 @@ import socket
 import sys
 import json
 
-from utils.constants import DEFAULT_PORT, MAX_CONNECTIONS
-from utils.message import get_message
+from utils.constants import DEFAULT_PORT, MAX_CONNECTIONS, RESPONSE, ERROR, ACTION, USER, ACCOUNT_NAME, PRESENCE, TIME, \
+    RESPONSE_DEFAULT_IP_ADDRESS
+from utils.messaging import Messaging
 
 
-class Server:
+class Server(Messaging):
     def __init__(self, ip_address='', port=DEFAULT_PORT):
+        super().__init__()
         self.ip_address = ip_address
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __str__(self):
         return f'Server is running on port {self.port}'
+
+    def parse_message(self, message):
+        if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+                and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+            return {RESPONSE: 200}
+        return {
+            RESPONSE_DEFAULT_IP_ADDRESS: 400,
+            ERROR: 'Bad Request'
+        }
 
     def listen(self):
         self.socket.bind((self.ip_address, self.port))
@@ -23,14 +34,24 @@ class Server:
         while True:
             client, client_addr = self.socket.accept()
             try:
-                message = get_message(client)
+                message = self.get_message(client)
                 print(message)
+                response = self.parse_message(message)
+                self.send_message(client, response)
             except (ValueError, json.JSONDecodeError):
-                print('Bad request from client')
+                print(f'Bad request from client: {client}')
             finally:
                 client.close()
 
 
 if __name__ == '__main__':
-    server = Server()
+    address = Messaging.get_address(sys.argv)
+    if address == -1:
+        sys.exit(1)
+
+    port = Messaging.get_port(sys.argv)
+    if port == -1:
+        sys.exit(1)
+
+    server = Server(address, port)
     server.listen()
